e567817d8c427622d36fd909182d34a7d1b2ce32
Alex Ang <angzhanyu@gmail.com>
2020-03-25 15:03:33 +0800

    Update wp-config-sample.php
    Use connection from dbCluster

--- src/php/wp-config-sample.php
+++ src/php/wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', getenv('DATABASE_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', getenv('DATABASE_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', getenv('DATABASE_PASS') );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', getenv('DATABASE_ENDPOINT') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -37,9 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-define('WP_SITEURL', 'https://[CLOUDFRONT DOMAIN NAME HERE]');
-define('WP_HOME', 'https://[CLOUDFRONT DOMAIN NAME HERE]');
-$_SERVER['HTTP_HOST'] = '[CLOUDFRONT DOMAIN NAME HERE]';
+define('WP_SITEURL', 'https://cloudfront_domain_name_here');
+define('WP_HOME', 'https://cloudfront_domain_name_here');
+$_SERVER['HTTP_HOST'] = 'cloudfront_domain_name_here';
 
 define( 'S3_UPLOADS_BUCKET', getenv('S3_UPLOADS_BUCKET') . '/wp-content' );
 define( 'S3_UPLOADS_USE_INSTANCE_PROFILE', true );
